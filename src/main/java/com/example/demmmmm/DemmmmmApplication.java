6cf44c875a29c4d5db584b1546e9bc9b7d7ed989
Demo <jayzilpilwar@gmail.com>
2024-05-07 22:04:16 +0530

    Cmmitted

---
+++ src/main/java/com/example/demmmmm/DemmmmmApplication.java
@@ -0,0 +1,13 @@
+package com.example.demmmmm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemmmmmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemmmmmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demmmmm/DemmmmmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demmmmm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemmmmmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
